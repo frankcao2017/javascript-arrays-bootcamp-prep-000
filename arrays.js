b99021206b08cdccab2cf83cd21a46e898377d1a
frankcao@me.com Cao <frankcao@me.com>
2018-06-19 12:21:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,5 +6,5 @@ function addElementToBeginningOfArray(chocolateBars, candyString) {
 }
 
 function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString) {
-  return chocolateBars.unshift("candyString")
+   chocolateBars.unshift("candyString")
 }
